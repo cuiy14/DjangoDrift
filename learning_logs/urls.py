09ce5a75e43a9bee4b722fcf309b49693477962f
drift <cuiy14@mails.tsinghua.edu.cn>
2017-04-09 11:21:51 +0800

    finished the multiple items page, and chapter 18

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^$',views.index,name='index'),
     #show all the topics
     url(r'^topics/$', views.topics, name='topics'),
+    #show the detailed page about a specific topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
