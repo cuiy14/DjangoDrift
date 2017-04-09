23246dce7bd6590802f2b62ca9897412f58a7ac7
drift <cuiy14@mails.tsinghua.edu.cn>
2017-04-09 18:45:19 +0800

    added the page for editing items

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     url(r'^new_topic/$',views.new_topic, name='new_topic'),
     #the page for adding new items
     url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    #the page for editing entry
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
