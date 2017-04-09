fdafc98615ef8ca886468294dfa14698c0825b86
drift <cuiy14@mails.tsinghua.edu.cn>
2017-04-09 09:26:05 +0800

    added the Entry model, registered it, and added some notes on the web server

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,16 @@ class Topic(models.Model):
     def __str__(self):
         """return the string form of the model"""
         return self.text
+       
+class Entry(models.Model):
+    """the concrete knowledge about a specific topic"""
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return self.text[:50] + "..."
+
